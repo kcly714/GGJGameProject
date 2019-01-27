c41786660e33920c241b6b338914b9155110d3a9
Jason Dillion <jadillion@gmail.com>
2019-01-27 15:33:35 -0800

    commiting sound

---
+++ Assets/Scripts/MusicControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicControl : MonoBehaviour
+{
+    public AudioMixerSnapshot Explore;
+    public AudioMixerSnapshot DifficultySleeping;
+    public float bpm = 128;
+
+    private float m_TransitionIn;
+    private float m_TransitionOut;
+    private float m_QuarterNote;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+        m_TransitionOut = m_QuarterNote;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("DifficultySleepingZone"))
+        {
+            DifficultySleeping.TransitionTo(m_TransitionIn);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("DifficultySleepingZone"))
+        {
+            Explore.TransitionTo(m_TransitionOut);
+        }
+    }
+}
